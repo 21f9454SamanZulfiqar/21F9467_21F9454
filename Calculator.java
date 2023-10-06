41ae49f7234802a05610e28dfc863afd3b42b158
21f9454SamanZulfiqar <145097319+21f9454SamanZulfiqar@users.noreply.github.com>
2023-10-06 17:09:08 +0500

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+
+public class Calculator {
+    public static void main(String[] args) {
+        double num1 = 10.5;  // First number
+        double num2 = 5.2;   // Second number
+
+        // Addition
+        double addition = num1 + num2;
+        System.out.println("Addition Result: " + addition);
+
+        // Subtraction
+        double subtraction = num1 - num2;
+        System.out.println("Subtraction Result: " + subtraction);
+    }
+}
+
